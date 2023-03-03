be0c2bcfc86dad820211c3e0747c595cb7dacd01
siddhartha verma <siddharthaverma6213@gmail.com>
2023-03-04 00:23:54 +0530

    React based netflix UI

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.scss"
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return(
+  <>
+   <Home/>
+  </>
+  );
+};
+
+export default App;
